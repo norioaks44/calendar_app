5253d6eb1a4e446cc73aab1820e12723e2806bff
norioaks44 <jrc.katou@gmail.com>
2020-11-24 15:39:54 +0900

    新規登録コントローラー設定

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    Event.create(event_parameter)
+    redirect_to root_path
+  end
+
+  private
+
+  def event_parameter
+    params.require(:event).permit(:today_id, :start_time, :time_id, :info, :coach_review)
+  end
+
 end
