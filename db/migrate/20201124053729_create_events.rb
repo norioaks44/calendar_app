bfc4c93a84f54a1ece353596e5ad0e3748e68a65
norioaks44 <jrc.katou@gmail.com>
2020-11-24 14:49:12 +0900

    first_commit

---
+++ db/migrate/20201124053729_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.integer :title_id
+      t.text :info
+      t.date :start_time
+      t.integer :time_id
+      t.text :coach_review
+      
+      t.timestamps
+    end
+  end
+end
